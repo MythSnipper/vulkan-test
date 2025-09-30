5d25286fe29b6e62d9a87a9113f747ff6021adc1
MythSnipper <mythsnipper2022@gmail.com>
2025-09-30 08:35:54 +0300

    doing the thing

---
+++ include/main.hpp
@@ -0,0 +1,13 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,67 @@
-#include <vulkan/vulkan.h>
-
-#include <iostream>
-#include <stdexcept>
-#include <cstdlib>
+#include <main.hpp>
 
 class HelloTriangleApplication{
     public:
     void run(){
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
     private:
+
+    GLFWwindow* window;
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+    VkInstance instance;
+
+    void initWindow(){
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan test nuck", nullptr, nullptr);
+        if(!window){
+            std::cout << "GLFW Failed to create window\n";
+        }
+    }
+    void createInstance(){
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+    }
+
     void initVulkan(){
-
+        createInstance();
     }
-
     void mainLoop(){
-
+        while(!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+        }
     }
-
     void cleanup(){
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+
+    
 };
 
 int main(){
+    #ifdef _WIN32
+        std::cout << "RUNNING ON WINDOWS\n";
+    #else
+        std::cout << "RUNNING ON LINUX\n";
+    #endif
+
+    
     HelloTriangleApplication app;
 
     try{
